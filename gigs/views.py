4a2287a01f7f0280104fe4322f90056b6d043a62
Gnanesh99 <gorlegnanesh.23.csd@anits.edu.in>
2026-07-29 15:59:08 +0530

    Implement Gig CRUD views

--- gigs/views.py
+++ gigs/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
-# Create your views here.
+from .models import Gig
+
+
+class GigListView(ListView):
+    model = Gig
+    template_name = 'gigs/gig_list.html'
+    context_object_name = 'gigs'
+
+
+class GigDetailView(DetailView):
+    model = Gig
+    template_name = 'gigs/gig_detail.html'
+    context_object_name = 'gig'
+
+
+class GigCreateView(LoginRequiredMixin, CreateView):
+    model = Gig
+    template_name = 'gigs/gig_form.html'
+    fields = ['title', 'description', 'category', 'budget', 'location']
+    success_url = reverse_lazy('gig-list')
+
+    def form_valid(self, form):
+        form.instance.poster = self.request.user
+        return super().form_valid(form)
+
+
+class GigUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Gig
+    template_name = 'gigs/gig_form.html'
+    fields = ['title', 'description', 'category', 'budget', 'location']
+    success_url = reverse_lazy('gig-list')
+
+    def test_func(self):
+        return self.get_object().poster == self.request.user
+
+
+class GigDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Gig
+    template_name = 'gigs/gig_confirm_delete.html'
+    success_url = reverse_lazy('gig-list')
+
+    def test_func(self):
+        return self.get_object().poster == self.request.user
